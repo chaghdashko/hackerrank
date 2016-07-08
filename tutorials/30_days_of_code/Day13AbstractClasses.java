087a460e12aea10199f5899ffe020cefea43d899
Cagdas Kocabiyik <chaghdasko@bitbucket.org>
2016-07-08 14:18:37 +0300

    day 13 - abstract classes

---
+++ tutorials/30_days_of_code/Day13AbstractClasses.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String t, String a) {
+        title = t;
+        author = a;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.printf("Title: %s\n", title);
+        System.out.printf("Author: %s\n", author);
+        System.out.printf("Price: %d\n", price);
+    }
+}
+
+class Day13AbstractClasses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        String author = sc.nextLine();
+        int price = sc.nextInt();
+        Book novel = new MyBook(title, author, price);
+        novel.display();
+    }
+}
